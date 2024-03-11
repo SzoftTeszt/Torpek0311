fe14589a29351e8bb0f93761b408f769efffdd11
Nagy János <nagy@zold.lan>
2024-03-11 16:18:25 +0100

    Add project files.

---
+++ Futtat/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TorpeAdatok.Data;
+using TorpeAdatok.Models;
+
+var _context = new Context();
+
+if (!_context.KiHolDolgozott.Any()) {
+    var sorok = File.ReadAllLines(@"c:\adat\kihol.csv").Skip(1);
+    foreach (var s in sorok)
+    {
+        _context.KiHolDolgozott.Add(new KiHolDolgozott(s));
+    }
+    _context.SaveChanges();
+}
+
+foreach (var item in _context.KiHolDolgozott)
+{
+    Console.WriteLine(item);
+}
+
+
